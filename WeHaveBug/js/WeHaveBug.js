6f4829eae91e4a5ab1889f9e75d304063f556ed2
Lian0123 <lienyungli123@gmail.com>
2018-07-28 16:54:44 +0800

    CrazyProgramer與WeHaveBug上傳

---
+++ WeHaveBug/js/WeHaveBug.js
@@ -0,0 +1,17 @@
+/*
+ *              << 這是個給該產品的每個後續維護員的話 >>
+ *
+ *                            一一一一
+ *           天 下 程 式       |- 皿 -|    都 會 有 BUG
+ *                   wv__   ㄒ|  免  |ㄒ   __vw
+ *                        \ | |  責  | | /
+ *                            |  聲  |
+ *                            |  明  |
+ *                             一一一一
+ * 
+ *    File Name         :  
+ *    File About        :  
+ *    The Program from  :  
+ *    For Next People   :  
+ * 
+ */
