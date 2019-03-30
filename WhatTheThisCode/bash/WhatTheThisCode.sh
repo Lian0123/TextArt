26a2378a48fb1e49df067240fcb1dbd50e346a8e
lian0123 <lienyungli123@gmail.com>
2019-03-30 23:24:10 +0800

    新增Bash版WhatTheThisCode

---
+++ WhatTheThisCode/bash/WhatTheThisCode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -e "\033[1;32m"
+
+echo "(O W O (O W O (O W O (O W O (O W O (O W O (O W O (O W O )"
+echo "(> H < (> H < (> H < (> H < (> H < (> H < (> H < (> H < )"
+echo "(- A - (- A - (- A - (- A - (- A - (- A - (- A - (- A - )"
+echo "(= T = (= T = (= T = (= T = (= T = (= T = (= T = (= T = )"
+echo "   ?      ?      ?      ?      ?      ?      ?      ?    "
+echo -e "\033[1;33m"
+echo "          這  是  啥  麼  鬼  代  碼  ～ ～ ～ ！        "
+echo -e "\033[1;32m"
+echo "(O W O (O W O (O W O (O W O (O W O (O W O (O W O (O W O )"
+echo "(> H < (> H < (> H < (> H < (> H < (> H < (> H < (> H < )"
+echo "(- A - (- A - (- A - (- A - (- A - (- A - (- A - (- A - )"
+echo "(= T = (= T = (= T = (= T = (= T = (= T = (= T = (= T = )"
+echo "   ?      ?      ?      ?      ?      ?      ?      ?    "
+echo -e "\033[0m"
